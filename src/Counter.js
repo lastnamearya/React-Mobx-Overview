d76b3064ee63be4ed79147ea7e62e018c9b8cea0
lastnamearya <arya.jigyasu6815@gmail.com>
2018-11-16 09:54:26 +0530

    Done with Counter in mobx

--- src/Counter.js
+++ src/Counter.js
@@ -2,7 +2,13 @@ import React from "react";
 import { observer } from "mobx-react";
 
 const Counter = ({ store }) => {
-  return <h2>{store.count}</h2>;
+  return (
+    <div>
+      <h2>{store.count}</h2>
+      <button onClick={store.addCount}>Increment</button>
+      <button onClick={store.subtractCount}>Decrement</button>
+    </div>
+  );
 };
 
 export default observer(Counter);
--- src/CounterStore.js
+++ src/CounterStore.js
@@ -1,7 +1,13 @@
 import { extendObservable, action } from "mobx";
 
 const CounterStore = extendObservable(this, {
-  count: 0
+  count: 0,
+  addCount: action(() => {
+    this.count += 1;
+  }),
+  subtractCount: action(() => {
+    this.count -= 1;
+  })
 });
 
 export default CounterStore;
